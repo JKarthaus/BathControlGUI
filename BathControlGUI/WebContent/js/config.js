47f15ccc6b305a0479f517349e01e0e47c141032
joern <joern@Tuxedo>
2016-12-17 19:04:45 +0100

    Add Javascript Files for mqttOverHttp and Jquery

---
+++ BathControlGUI/WebContent/js/config.js
@@ -0,0 +1,10 @@
+
+host = 'bathcontrol';	// hostname or IP address
+
+port = 9001;
+topic = '#';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+
+cleansession = true;
