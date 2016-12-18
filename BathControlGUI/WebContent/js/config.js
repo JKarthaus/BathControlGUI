1006ba6e6716592c6f0897a9718268a7c22a3896
joern <joern@Tuxedo>
2016-12-18 19:29:23 +0100

    Add many UI Bugfixes

--- BathControlGUI/WebContent/js/config.js
+++ BathControlGUI/WebContent/js/config.js
@@ -2,7 +2,7 @@
 host = 'bathcontrol';	// hostname or IP address
 
 port = 9001;
-topic = '#';		// topic to subscribe to
+topic = 'bathcontrol/gui';		// topic to subscribe to
 useTLS = false;
 username = null;
 password = null;
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BathControlGUI
 The BathControl Web GUI
+
+
+## Test JSON 
+'''
+{"tempBadezimmer":22.3,"tempWintergarten":29.2,"tempAussen":5.6,"feuchteBad":80,"statusZuluft":"ON","statusAbluft":"OFF"}
+'''
