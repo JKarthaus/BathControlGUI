380b3f2ab146bf096961ddc6d18f68567e082d03
joern <joern@Tuxedo>
2016-12-29 22:51:25 +0100

    Some Minor config changes

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -2,7 +2,7 @@
 host = 'bathcontrol';	// hostname or IP address
 
 port = 9001;
-topic = 'bathcontrol/gui';		// topic to subscribe to
+topic = 'bathcontrolGui';		// topic to subscribe to
 useTLS = false;
 username = null;
 password = null;
